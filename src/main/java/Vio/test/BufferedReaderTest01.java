ef4d7ebf8c85cd139452098c548b2e6035a46bb5
Paulo Rubens <paulorubensbarbosa@outlook.com>
2025-07-21 22:23:03 -0300

    aula 142 - Classes Utilitárias - BufferedReader

---
+++ src/main/java/Vio/test/BufferedReaderTest01.java
@@ -0,0 +1,24 @@
+package Vio.test;
+
+import java.io.*;
+
+public class BufferedReaderTest01 {
+    public static void main(String[] args) {
+
+        File file = new File("D:\\WorkSpace\\Curso-DevDojo-Java\\untitled\\arquivo\\file.txt");
+
+        try (FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr)){
+
+            String linha;
+            while ((linha = br.readLine()) != null){
+                System.out.println(linha);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
