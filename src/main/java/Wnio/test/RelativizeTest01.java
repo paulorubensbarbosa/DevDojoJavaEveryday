8cba672a193a78d9d022e474dc099c42679040d5
Paulo Rubens <paulorubensbarbosa@outlook.com>
2025-07-25 21:12:49 -0300

    aula 148 - Classes Utilitárias - NIO Relativize

---
+++ src/main/java/Wnio/test/RelativizeTest01.java
@@ -0,0 +1,31 @@
+package Wnio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class RelativizeTest01 {
+    public static void main(String[] args) {
+        //como que faz para chegar no path 2 a partir do path 1
+
+        Path dir = Paths.get("/home/Paulo");
+        Path clazz = Paths.get("/home/Paulo/devdojofoda/olaMundo.java");
+
+        Path pathToClazz = dir.relativize(clazz);
+        //System.out.println(pathToClazz);
+
+        Path absoluto1 = Paths.get("/home/Paulo");
+        Path absoluto2 = Paths.get("/usr/local");
+        Path absoluto3 = Paths.get("/home/Paulo/devdojofoda/olaMundo.java");
+        Path relativo1 = Paths.get("temp");
+        Path relativo2 = Paths.get("temp/temp.22043910");
+
+        System.out.println("1 " + absoluto1.relativize(absoluto3));
+        System.out.println("2 " + absoluto3.relativize(absoluto1));
+        System.out.println("3 " + absoluto1.relativize(absoluto2));
+        System.out.println("4 " + relativo1.relativize(relativo2));
+        System.out.println("5 " + absoluto1.relativize(relativo1));
+
+
+
+    }
+}
--- src/main/java/Wnio/test/ResolveTest01.java
+++ src/main/java/Wnio/test/ResolveTest01.java
@@ -1,11 +1,10 @@
 package Wnio.test;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class PathTest03 {
+public class ResolveTest01 {
     public static void main(String[] args) throws IOException {
 
 
