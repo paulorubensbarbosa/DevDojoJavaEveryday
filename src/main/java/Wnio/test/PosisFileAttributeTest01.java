9754efcc98bb615d0649bbae34985e03e816108c
Paulo Rubens <paulorubensbarbosa@outlook.com>
2025-07-28 23:34:32 -0300

    Aula 151 - Classes Unitarias PosisFileAttribute classe para gerenciar arquivos no linux

---
+++ src/main/java/Wnio/test/PosisFileAttributeTest01.java
@@ -0,0 +1,26 @@
+package Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFileAttributeView;
+import java.nio.file.attribute.PosixFileAttributes;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Set;
+
+public class PosisFileAttributeTest01 {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("test.txt");
+        PosixFileAttributes posixFileAttributes = Files.readAttributes(path, PosixFileAttributes.class);
+
+        PosixFileAttributeView fileAttributeView = Files.getFileAttributeView(path, PosixFileAttributeView.class);
+        Set<PosixFilePermission> posixFilePermissions = PosixFilePermissions.fromString("rw-rw-rw-");
+
+        fileAttributeView.setPermissions(posixFilePermissions);
+
+        System.out.println(fileAttributeView.readAttributes().permissions());
+
+    }
+}
