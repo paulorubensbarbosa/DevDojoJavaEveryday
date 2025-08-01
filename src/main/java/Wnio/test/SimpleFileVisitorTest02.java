4fb8d842f16976b78ce4348daf7ce71b19ac0d37
Paulo Rubens <paulorubensbarbosa@outlook.com>
2025-07-31 22:42:30 -0300

    Aula 154 - Classes Unitarias SimpleFileVisitor - acesso a todas as pastas

--- src/main/java/Wnio/test/SimpleFileVisitorTest02.java
+++ src/main/java/Wnio/test/SimpleFileVisitorTest02.java
@@ -8,7 +8,7 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 //Configuração para retornar todos os arquivos com o final .Java
-class ListJavaFiles extends SimpleFileVisitor<Path> {
+class ListAllFiles extends SimpleFileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs){
         if(file.getFileName().toString().endsWith(".java")) {
@@ -16,10 +16,27 @@ class ListJavaFiles extends SimpleFileVisitor<Path> {
         }
         return FileVisitResult.CONTINUE;
     }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        System.out.println("pre visit " + dir.getFileName());
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return super.visitFileFailed(file, exc);
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        System.out.println("post visit " + dir.getFileName());
+        return FileVisitResult.CONTINUE;
+    }
 }
 
 
-public class SimpleFileVisitorTest01 {
+public class SimpleFileVisitorTest02 {
     public static void main(String[] args) throws IOException {
         Path path = Paths.get("pasta");
 
@@ -32,9 +49,9 @@ public class SimpleFileVisitorTest01 {
 
         System.out.println(localDateTime.format(dateTimeFormatter));
 
-        Path root = Paths.get(".");
+        Path root = Paths.get("pasta");
 
-        Files.walkFileTree(root, new ListJavaFiles());
+        Files.walkFileTree(root, new ListAllFiles());
 
     }
 }
