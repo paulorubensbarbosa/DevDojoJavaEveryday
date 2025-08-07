3ffad87a472ed639406a2cac23ae0b8cae0c2615
Paulo Rubens <paulorubensbarbosa@outlook.com>
2025-08-06 23:54:47 -0300

    Aula 161 - Colecoes - Equals pt2

--- src/main/java/Ycolecoes/dominio/Smartphone.java
+++ src/main/java/Ycolecoes/dominio/Smartphone.java
@@ -6,7 +6,7 @@ public class Smartphone {
     private String serialNumber;
     private String marca;
 
-    public Smartphone(String marca, String serialNumber) {
+    public Smartphone(String serialNumber, String marca) {
         this.marca = marca;
         this.serialNumber = serialNumber;
     }
@@ -27,11 +27,18 @@ public class Smartphone {
         this.marca = marca;
     }
 
+    //Reflexivo x.equals(x) tem que ser true para tudo que for diferente de null
+    //Simétrico para x e y diferentes de null, se x.equals(y) == true, logo, y.equals(x) == true
+    //Transitividade para x, y, z diferentes de null, se x.equals(y) == true, e x.equals(z)==true,  logo y.equals(z) ==true
+    //Consistente x.equals(x) sempre retorna true se x for diferente de null
+    //para x diferente de null x.equals(null) tem que retornar false
     @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Smartphone that = (Smartphone) o;
-        return Objects.equals(serialNumber, that.serialNumber) && Objects.equals(marca, that.marca);
+    public boolean equals(Object object) {
+        if(object == null) return  false;
+        if(this == object) return true;
+        if(this.getClass() != object.getClass()) return false;
+        Smartphone smartphone = (Smartphone) object;
+        return serialNumber != null && serialNumber.equals(smartphone.serialNumber);
     }
 
     @Override
