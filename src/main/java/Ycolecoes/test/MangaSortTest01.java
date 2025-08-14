0ea2ca627eb036328e93d6f1c187234cfcd7a0e1
Paulo Rubens <paulorubensbarbosa@outlook.com>
2025-08-13 22:20:03 -0300

    Aula 170 - Colecoes - Sorting Comparable

--- src/main/java/Ycolecoes/test/MangaSortTest01.java
+++ src/main/java/Ycolecoes/test/MangaSortTest01.java
@@ -4,7 +4,22 @@ import Ycolecoes.dominio.Manga;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+class MangaByIdComparator implements Comparator<Manga>{
+    @Override
+    public int compare(Manga manga1, Manga manga2) {
+        return manga1.getId().compareTo(manga2.getId());
+    }
+}
+
+class MangaByPreco implements Comparator<Manga>{
+    @Override
+    public int compare(Manga o1, Manga o2) {
+        return Double.compare(o1.getPreco(), o2.getPreco());
+    }
+}
 
 public class MangaSortTest01 {
     public static void main(String[] args) {
@@ -27,5 +42,19 @@ public class MangaSortTest01 {
             System.out.println(manga);
         }
 
+        System.out.println("------------------------------------------------");
+
+        Collections.sort(mangas, new MangaByIdComparator());
+
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
+
+        System.out.println("------------------------------------------------");
+
+        Collections.sort(mangas, new MangaByPreco());
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
     }
 }
