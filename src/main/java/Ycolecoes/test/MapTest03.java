e9baa67a31568504baa1bbc85b92e0aeeb2188ee
Paulo Rubens <paulorubensbarbosa@outlook.com>
2025-08-24 22:51:07 -0300

    Aula 180 - Colecoes - Map, LinkedHashMap pt3

--- src/main/java/Ycolecoes/test/MapTest03.java
+++ src/main/java/Ycolecoes/test/MapTest03.java
@@ -3,11 +3,9 @@ package Ycolecoes.test;
 import Ycolecoes.dominio.Consumidor;
 import Ycolecoes.dominio.Manga;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-public class MapTest02 {
+public class MapTest03 {
     public static void main(String[] args) {
         Consumidor consumidor1 = new Consumidor("Paulo");
         Consumidor consumidor2 = new Consumidor("DevDojo Academy");
@@ -21,9 +19,22 @@ public class MapTest02 {
         Map<Consumidor, Manga> consumidorMangaMap = new LinkedHashMap<>();
         consumidorMangaMap.put(consumidor1, manga1);
         consumidorMangaMap.put(consumidor2, manga4);
-        Set<Map.Entry<Consumidor, Manga>> entries = consumidorMangaMap.entrySet();
-        for (Map.Entry<Consumidor, Manga> entry : entries) {
-            System.out.println(entry.getKey().getNome() + ": " + entry.getValue().getNome());
+
+        List<Manga> consumidor1List = List.of(manga1, manga2, manga3);
+        List<Manga> consumidor2List = List.of(manga3, manga4);
+
+        Map<Consumidor, List<Manga>> consumidorMaga = new LinkedHashMap<>();
+
+        consumidorMaga.put(consumidor1, consumidor1List);
+        consumidorMaga.put(consumidor2, consumidor2List);
+
+        for (Map.Entry<Consumidor, List<Manga>> entry : consumidorMaga.entrySet()) {
+            System.out.println(entry.getKey().getNome());
+            for (Manga manga : entry.getValue()){
+                System.out.println(manga.getNome());
+            }
         }
+
+
     }
 }
