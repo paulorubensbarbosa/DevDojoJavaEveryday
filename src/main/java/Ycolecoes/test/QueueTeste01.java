ad93446aa4bf076e4ea8a1747a72362f6c5a51b0
Paulo Rubens <paulorubensbarbosa@outlook.com>
2025-08-26 23:44:52 -0300

    Aula 181 - Colecoes - Queue pt1

---
+++ src/main/java/Ycolecoes/test/QueueTeste01.java
@@ -0,0 +1,10 @@
+package Ycolecoes.test;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueTeste01 {
+    public static void main(String[] args) {
+        Queue<String> fila = new PriorityQueue<>();
+    }
+}
