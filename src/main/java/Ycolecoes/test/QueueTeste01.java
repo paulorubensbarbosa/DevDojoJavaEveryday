77822684ce9839f541131989213585035a4dcfb2
Paulo Rubens <paulorubensbarbosa@outlook.com>
2025-08-27 23:05:31 -0300

    Aula 181 - Colecoes - Map, NavigableMap, ThreeMap pt3

--- src/main/java/Ycolecoes/test/QueueTeste01.java
+++ src/main/java/Ycolecoes/test/QueueTeste01.java
@@ -1,10 +1,26 @@
 package Ycolecoes.test;
 
+import Ycolecoes.dominio.Manga;
+
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
+class sortMangaById implements Comparator<Manga>{
+    @Override
+    public int compare(Manga o1, Manga o2) {
+        return o1.getId().compareTo(o2.getId());
+    }
+}
+
 public class QueueTeste01 {
     public static void main(String[] args) {
-        Queue<String> fila = new PriorityQueue<>();
+        //First in, first out
+        Queue<String> fila = new PriorityQueue<>(sortMangaById);
+        fila.add("C");
+        fila.add("B");
+        fila.add("A");
+
+
     }
 }
