c6e68b6dd9ae3f8cad57235881f5384ddc8e8928
Paulo Rubens <paulorubensbarbosa@outlook.com>
2025-09-17 22:43:36 -0300

    Aula 196 - Lambdas pt 2

--- src/main/java/ZZClambdas/test/LambdaTest01.java
+++ src/main/java/ZZClambdas/test/LambdaTest01.java
@@ -1,7 +1,19 @@
 package ZZClambdas.test;
 
 
+import java.util.List;
+import java.util.function.Consumer;
+
 public class LambdaTest01 {
     public static void main(String[] args) {
+        List<String> nomes = List.of("Paulo", "Rubens");
+        List<Integer> inteiros = List.of(1, 2, 3, 4, 5);
+        forEach(nomes, c -> System.out.println(c) );
+        forEach(inteiros, c -> System.out.println(c) );
+    }
+    private static <T> void forEach (List<T> list, Consumer<T> consumer){
+        for (T t : list) {
+            consumer.accept(t);
+        }
     }
 }
